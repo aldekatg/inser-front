2c7ab07b2a4bbcdca99f121992b6ab13e91b9e13
atagay <atagay@beeline.kz>
2025-08-19 16:42:56 +0500

    build

--- src/api/auth/index.ts
+++
@@ -1,8 +0,0 @@
-export function refreshAccessToken(
-  refreshToken: string
-): Promise<void> {
-  console.log(refreshToken)
-  return new Promise((resolve, _) => {
-    setTimeout(() => resolve, 5000)
-  })
-}
